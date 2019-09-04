1ec3de074711fb21f281bfe2884be185d0bc6e99
Nick Stephens <Nicholas.J.Stephens-1@ou.edu>
2019-09-04 11:05:52 -0500

    datetime

---
+++ Participation/Participation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            DateTime future = today.AddDays(27);
+            Console.WriteLine(today.ToLongDateString());
+            Console.WriteLine(future);
+
+            //shows the difference between the 2 days
+            var difference = future - today;
+            Console.WriteLine(difference.Days); //difference._____ can retrieve which ever value you want. (days, time, seconds, etc.)
+
+            Console.ReadKey();
+        }
+    }
+}
